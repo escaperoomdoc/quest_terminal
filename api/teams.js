37223e0801796919f178a5b85e2aa95a324275f9
Alex Chiga <chiga@protonmail.com>
2020-02-04 19:51:41 +0300

    changed text of error

--- api/teams.js
+++ api/teams.js
@@ -16,7 +16,7 @@ async function apiTeamGet(app, req, res) {
 		res.status(200).json(result);
 	}
 	catch(error) {
-		return res.status(400).json({error: 'get /api/teams: ' + error});
+		return res.status(400).json({error: 'get /api/team: ' + error});
 	}
 }
 
