96bf346230c36bef1e1eef9c3f4aa11e870e2b9f
Alex Chiga <chiga@protonmail.com>
2020-03-13 11:42:09 +0300

    added reboot and shutdown scripts

---
+++ shutdown.bat
@@ -0,0 +1,15 @@
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.11 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.12 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.13 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.14 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.15 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.16 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.17 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.18 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.19 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.20 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.21 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.22 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.23 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.24 -pw erd sudo poweroff
+start /min C:\"Program Files"\PuTTY\plink.exe -batch -ssh pi@192.168.100.25 -pw erd sudo poweroff
